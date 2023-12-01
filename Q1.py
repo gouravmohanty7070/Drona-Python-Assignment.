193665ee21fcbc79884ad4e18def5e8a894d5857
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-12-01 19:21:46 +0530

    feat: Drone Movement System Added

--- Q1.py
+++ Q1.py
@@ -1,1 +1,29 @@
+import turtle
 
+screen = turtle.Screen()
+drone = turtle.Turtle()
+
+
+def move_forward():
+    drone.forward(50)
+
+
+def move_backward():
+    drone.backward(50)
+
+
+def move_left():
+    drone.left(90)
+
+
+def move_right():
+    drone.right(90)
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+
+screen.mainloop()
